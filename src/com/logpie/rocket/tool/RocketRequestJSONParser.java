397bbb7e255ded379410b612f8cf49110d1a639a
yilei <zyl.echo@gmail.com>
2014-03-25 00:20:06 -0700

    build jsonParser and MetricRecord

---
+++ src/com/logpie/rocket/tool/RocketRequestJSONParser.java
@@ -0,0 +1,15 @@
+package com.logpie.rocket.tool;
+
+import org.json.JSONObject;
+
+import com.logpie.rocket.MetricRecord;
+
+public class RocketRequestJSONParser {
+
+	public static MetricRecord constructMandatoryParameter(JSONObject requestJSON)
+	{
+		MetricRecord metricRecord = new MetricRecord();
+		return metricRecord;
+		
+	}
+}
